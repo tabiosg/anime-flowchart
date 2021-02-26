fa543b8e46807f32e826feda19fae6257f03a531
tabiosg <gutstabios@gmail.com>
2021-02-26 14:09:37 -0500

    Fix code for one list

--- code/readlist.py
+++ code/readlist.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import re
 import statistics as s
-import xml.etree.ElementTree as et 
+import xml.etree.ElementTree as et
+
+def sort(df):
+    df = df.sort_values(by=['my_score', 'series_type', 'my_status'])
+    return df
 
 def main():
     xtree = et.parse("../data/gugilta.xml")
@@ -12,19 +16,20 @@ def main():
     j = 0;
     for node in xroot:
         if j != 0:
-            s_title = node.attrib.get("series_title").text if node is not None else None
-            s_type = node.attrib.get("series_type").text if node is not None else None
-            s_episodes = node.find("series_episodes").text if node is not None else None
-            s_score = node.find("my_score").text if node is not None else None
-            s_status = node.find("my_status").text if node is not None else None
+            s_title = node.find("series_title").text if node is not None else None
+            s_type = node.find("series_type").text if node is not None else None
+            s_episodes = node.find("series_episodes").text
+            s_score = node.find("my_score").text
+            s_status = node.find("my_status").text
     
             rows.append({"series_title": s_title, "series_type": s_type, "series_episodes": s_episodes, 
                  "my_score": s_score, "my_status": s_status})
         j = j + 1
 
-    out_df = pd.DataFrame(rows, columns = df_cols)
-    
-    out_df.to_csv('../data/gugilta_clean.csv')
+    df = pd.DataFrame(rows, columns = df_cols)
+    df = sort(df)
+
+    df.to_csv('../data/gugilta_clean.csv')
 
 if __name__ == "__main__":
     main()
