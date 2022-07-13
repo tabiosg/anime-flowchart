a09b9a0a476003e95396bf715135fce10aa4b3cb
Guthrie <71603173+tabiosg@users.noreply.github.com>
2022-07-13 10:29:38 -0400

    Change all var to const or let

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ import { map } from './raw_data.js';
 
 /*Store commonly used variables*/
 
-var choiceButtonArray = [
+let choiceButtonArray = [
     document.getElementById("choice-zero"),
     document.getElementById("choice-one"),
     document.getElementById("choice-two"),
@@ -12,7 +12,7 @@ var choiceButtonArray = [
     document.getElementById("choice-six"),
     document.getElementById("choice-seven")
 ]
-var prompt = document.getElementById("prompt-square");
+let prompt = document.getElementById("prompt-square");
 const reloadButton = document.querySelector("#restart-button");
 
 
@@ -20,15 +20,15 @@ reloadButton.addEventListener("click", function(){location.reload()}, false);
 
 /*Handle everything going back here*/
 
-var historyStack = [];
+let historyStack = [];
 
 
-var goBackButton = document.getElementById("previous-question-button");
+let goBackButton = document.getElementById("previous-question-button");
 goBackButton.onclick = function () { 
     if (historyStack.length == 0) {
         return;
     }
-    var rewindToThisPrompt = historyStack.pop();
+    let rewindToThisPrompt = historyStack.pop();
     prompt.innerHTML = rewindToThisPrompt;
     SetUpButtons();
 };
@@ -36,17 +36,17 @@ goBackButton.onclick = function () {
 /*Handle all interactions when clicked*/
 
 function SetUpButtons() {
-    for (var i = 0; i < 8; ++i) {
+    for (let i = 0; i < 8; ++i) {
         choiceButtonArray[i].style.display = "none";
     }
-    var promptQuestion = prompt.innerHTML;
-    var theArray = map.get(promptQuestion);
+    let promptQuestion = prompt.innerHTML;
+    const theArray = map.get(promptQuestion);
 
     if (theArray.length == 3) return;
 
-    for (var i = 2; i < theArray.length; ++i) {
-        var choiceArray = theArray[i];
-        var choiceString = choiceArray[0];
+    for (let i = 2; i < theArray.length; ++i) {
+        const choiceArray = theArray[i];
+        const choiceString = choiceArray[0];
         choiceButtonArray[i - 2].innerHTML = choiceString;
         choiceButtonArray[i - 2].style.display = "inline-block";
     }
@@ -61,15 +61,10 @@ var changeButtonArray = Array.apply(null, Array(8)).map(function () {})
 
 for (let i = 0; i < changeButtonArray.length; ++i) {
     changeButtonArray[i] = function() {
-        console.log("this is i")
-        console.log(i)
-        let j = i;
-        console.log("this is j")
-        console.log(j)
-        var buttonComponentPressed = choiceButtonArray[j];
-        var answerSelectedString = buttonComponentPressed.innerHTML;
-        var theArrayNext = map.get(answerSelectedString);
-        var changePrompt = theArrayNext[1];
+        const buttonComponentPressed = choiceButtonArray[i];
+        const answerSelectedString = buttonComponentPressed.innerHTML;
+        const theArrayNext = map.get(answerSelectedString);
+        const changePrompt = theArrayNext[1];
         historyStack.push(prompt.innerHTML);
         prompt.innerHTML = theArrayNext[0];
         SetUpButtons();
@@ -77,6 +72,6 @@ for (let i = 0; i < changeButtonArray.length; ++i) {
     }
 }
 
-for (var i = 0; i < choiceButtonArray.length; ++i) {
+for (let i = 0; i < choiceButtonArray.length; ++i) {
     choiceButtonArray[i].onclick = changeButtonArray[i];
 }
